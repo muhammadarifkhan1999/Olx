8fca7625db27405381dc7a88485e04594c2cb1ff
Muhammad Arif Khan <muhammadkhanzada1999@gmail.com>
2022-10-01 22:02:15 +0500

    first commit

---
+++ publlic/olx.js
@@ -0,0 +1,123 @@
+
+
+// var Girl= prompt("What is your name?")
+// var Boy= prompt("what is your age?")
+// var Custom= prompt("what do you want from us?")
+// console.log(Girl+" "+" "+Boy+" "+Custom)
+
+// var Girl= prompt("What is your name?")
+// var Boy= prompt("what is your age?")
+// var Custom= prompt("what do you want from us?")
+// console.log(Girl*" "*" "*Boy*" "*Custom)
+
+// var Girl= prompt("What is your name?")
+// var Boy= prompt("what is your age?")
+// var Custom= prompt("what do you want from us?")
+// console.log(Girl/" "/" "/Boy/" "/Custom)
+// var num1= prompt("enter num here")
+// var num2= prompt("enter num here")
+// alert (num1 + num2)
+// var num= prompt("Please Enter Number Here")
+// if (num>=5&&num<=10){
+//     alert("Wellcome")
+// }
+
+// else if (num>10){
+//     alert("Smartly Wellcome")
+// }
+// else {
+//     alert("Get Lost")
+// }
+// var enternumber=prompt("Enter Number ")
+// var store1 =[]
+// for (var i=1;i<=enternumber;i++){
+//     var input = prompt("Write There Names")
+//     store1.push(input)
+// }
+// alert(store1)
+// var entername=prompt("Wirte Name for index")
+// var store2=store1.indexOf(entername)
+// alert(store2)
+// var show=prompt("Please Enter Number Here")
+// show=show.toLowerCase()
+// var store1=[]
+// for (var i=1;i<=show;i++){
+//     var enter=prompt("Please Enter Name Here")
+//     store1.push(enter)
+// }
+// alert(store1)
+// var show=prompt("Please Enter The Name For Index")
+// var store2=store1.indexOf(show)
+// alert(store2)
+var con=prompt("Sir What's Your Good Name?")
+con=con.toLowerCase()
+alert("Hello" +" "+con)
+var con1=prompt("How Are You Bro?")
+ con1=con1.toLowerCase()
+if (con1 == "fine"){
+    alert("Good")
+}
+else if(con1 == "not good"){
+    alert("God Bless You Bro")
+}
+var con2=prompt("How Old You Are Bro")
+var con3=prompt("Your Qualification ?")
+ con3=con3.toLowerCase()
+if (con3 === "matric"){
+    alert("Good")
+}
+else if(con3 == "inter"){
+alert("Very Good")
+}
+else if(con3 == "graduation"){
+    alert("excellent")
+}
+var con4=prompt("Do you Have a Job?")
+ con4=con4.toLowerCase()
+if (con4 == "yes"){
+    alert("good")
+    var con5=prompt("is your job timing is 9to5?")
+    con5=con5.toLowerCase()
+    if (con5 == "yes"){
+        alert("very good")
+        alert("You Are Such A Hard Working Person")
+
+    }
+    else if (con5 == "no"){
+        alert("so?")
+    var con5=prompt("do your work for 12 hours?")
+    con5=con5.toLowerCase()
+    if (con5 == "yes"){
+        alert("nice")
+        alert("You Are Such A Hard Working Person")
+
+    }
+    else if(con5 == "no"){
+        alert("what rubbish bro")
+        alert("Layer You Have no job")
+    }
+}
+    
+}
+else if(con4 == "no"){
+    alert("hard luck for you bro")
+}
+// var con5=prompt("Do You Have Facebook Account?")
+// if (con5 == "yes"){
+// var con11=prompt("do you like to chatting on messenger?")
+// if (con11 == "yes"){
+// }
+//     alert("Good")
+// }
+// else if(con5 =="no"){
+//     alert("Fine")
+// }
+// // var con6=prompt("Do You Like to Chating on Facebook?")
+// var con7=prompt("Do You Have Whatsapp In Your Mobile Phone?")
+// var con8=prompt("Do You Like to Chating or Video Call On whatsapp Late Night?")
+// var con9=prompt("Do You Have Any Number or photo of any Strenger lady in your cell phone?")
+// alert("Layer You Have A Girl Friend")
+// }
+// else if (con3 == "yes"){
+//     alert("You Are Such a Honest Bro")
+// }
